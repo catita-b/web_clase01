3e347b7d0f60abc7aa6ab29cfda95a002f67c67e
Catalina Bustos <catitabustos2003@gmail.com>
2025-08-14 20:14:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
+var d; 
 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255,0,0);
+  frameRate(100);  
+}
+
+function draw() {
+  d = random (10,100);
+  noStroke();
+  fill(255,d);
+  ellipse (mouseX, mouseY ,d,d);
+}
